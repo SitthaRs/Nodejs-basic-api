d0d43bdf9b3ac3e5af9bcc29cbcc389a88611490
reworld <reworldt@gmail.com>
2019-09-28 00:24:11 +0700

    fix travis

---
+++ test/app.test.js
@@ -0,0 +1,11 @@
+let mongoose = require("mongoose");
+
+before(function(done) {
+  function clearCollections() {
+    for (var collection in mongoose.connection.collections) {
+      mongoose.connection.dropCollection(collection, function(err, result) {});
+    }
+    return done();
+  }
+  return clearCollections();
+});
